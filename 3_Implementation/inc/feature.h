9f7397a7c7ed2e7cc91bc3236d6dcbbd4d5691b5
D1nesh12 <102590808+D1nesh12@users.noreply.github.com>
2022-04-10 22:09:15 +0530

    Add files via upload

---
+++ 3_Implementation/inc/feature.h
@@ -0,0 +1,19 @@
+
+
+#ifndef FEATURE_H_
+#define FEATURE_H_
+
+
+int cagr();
+int discountcalculator();
+int ppf();
+int emi();
+int rd();
+int fd();
+int lumpsum();
+int sip();
+
+
+
+#define MACRO -99
+#endif
